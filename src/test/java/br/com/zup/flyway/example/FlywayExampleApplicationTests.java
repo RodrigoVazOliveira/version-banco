7780e4f94bfd1e09537a4a6cec14b9d873def1f1
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-05-03 07:58:38 -0300

    created project

---
+++ src/test/java/br/com/zup/flyway/example/FlywayExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.flyway.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
